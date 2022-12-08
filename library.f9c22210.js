75a575654d37938d99fbf4165ace4df1c99edad1
ElenaOds <106108433+ElenaOds@users.noreply.github.com>
2022-12-08 12:53:37 +0000

    Deploying to gh-pages from @ Serhii26/filmoteka@83d888671cec02e340f3f9e7eacb1f5c322cc7a2 🚀

--- library.f9c22210.js
+++ library.f9c22210.js
@@ -1,2 +1,2 @@
 !function(){var e,t=document.querySelector("body"),c=document.querySelector("#codepen"),s="light-theme",a="dark-theme";(e=localStorage.getItem("Theme"))===a?(t.classList.add(e),c.checked=!0):t.classList.add(s),c.addEventListener("change",(function(){t.classList.toggle(a),t.classList.toggle(s),e=t.classList,localStorage.setItem("Theme",e);var e}))}();
-//# sourceMappingURL=index.9504bd89.js.map
+//# sourceMappingURL=library.f9c22210.js.map
